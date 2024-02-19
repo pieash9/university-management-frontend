0e33084b80b0c86e7cfaca43d28a601e10768969
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-02-19 13:04:17 +0600

    configure antd and added layout

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>Error! Page not found!</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
---
+++ src/components/ui/Contents.tsx
@@ -0,0 +1,9 @@
+"use clients";
+
+import { Content } from "antd/es/layout/layout";
+
+const Contents = ({ children }: { children: React.ReactNode }) => {
+  return <Content style={{ minHeight: "100vh" }}>{children}</Content>;
+};
+
+export default Contents;
