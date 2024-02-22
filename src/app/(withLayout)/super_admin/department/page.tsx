701cc459cd1d819715883764fc25352a86c3ec1e
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-02-22 22:08:13 +0600

    implement table at department

--- src/app/(withLayout)/super_admin/department/page.tsx
+++ src/app/(withLayout)/super_admin/department/page.tsx
@@ -1,8 +1,81 @@
+"use client";
+
 import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
+import UMTable from "@/components/ui/UMTable";
+import { useDepartmentsQuery } from "@/redux/api/departmentApi";
 import { Button } from "antd";
 import Link from "next/link";
+import { useState } from "react";
+import { DeleteOutlined, EditOutlined, EyeOutlined } from "@ant-design/icons";
 
 const ManageDepartmentPage = () => {
+  const [size, setSize] = useState<number>(10);
+  const [page, setPage] = useState<number>(1);
+  const [sortBy, setSortBy] = useState<string>("");
+  const [sortOrder, setSortOrder] = useState<string>("");
+
+  const query: Record<string, any> = {};
+
+  query["limit"] = size;
+  query["page"] = page;
+  query["sortBy"] = sortBy;
+  query["sortOrder"] = sortOrder;
+
+  const { data, isLoading } = useDepartmentsQuery({ ...query });
+
+  const departments = data?.departments;
+  const meta = data?.meta;
+
+  const columns = [
+    {
+      title: "Title",
+      dataIndex: "title",
+    },
+    {
+      title: "CreatedAt",
+      dataIndex: "createdAt",
+      sorter: true,
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+    {
+      title: "Action",
+      render: (data: any) => (
+        <>
+          <Button onClick={() => console.log(data)} type="primary">
+            <EditOutlined />
+          </Button>
+          <Button
+            style={{ margin: "0 5px" }}
+            onClick={() => console.log(data)}
+            type="primary"
+          >
+            <EyeOutlined />
+          </Button>
+          <Button onClick={() => console.log(data)} type="primary" danger>
+            <DeleteOutlined />
+          </Button>
+        </>
+      ),
+    },
+  ];
+
+  const onPaginationChange = (page: number, pageSize: number) => {
+    // console.log({ page, pageSize });
+    setPage(page);
+    setSize(pageSize);
+  };
+
+  const onTableChange = (pagination: any, filter: any, sorter: any) => {
+    const { order, field } = sorter;
+    // console.log(order, field);
+    setSortBy(field as string);
+    setSortOrder(order === "ascend" ? "asc" : "desc");
+  };
+
   return (
     <div>
       <UMBreadCrumb
@@ -17,6 +90,17 @@ const ManageDepartmentPage = () => {
       <Link href="/super_admin/department/create">
         <Button type="primary">Create</Button>
       </Link>
+      <UMTable
+        loading={isLoading}
+        columns={columns}
+        dataSource={departments}
+        pageSize={size}
+        totalPages={meta?.total}
+        showSizeChanger={true}
+        onPaginationChange={onPaginationChange}
+        onTableChange={onTableChange}
+        showPagination={true}
+      />
     </div>
   );
 };
---
+++ src/redux/api/departmentApi.ts
@@ -0,0 +1,35 @@
+import { IDepartment, IMeta } from "@/types";
+import { tagTypes } from "../tagTypes";
+import { baseApi } from "./baseApi";
+
+const DEPARTMENT_URL = "/management-departments";
+
+export const departmentApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    departments: build.query({
+      query: (arg: Record<string, any>) => ({
+        url: DEPARTMENT_URL,
+        method: "GET",
+        params: arg,
+      }),
+      transformResponse: (response: IDepartment, meta: IMeta) => {
+        return {
+          departments: response,
+          meta,
+        };
+      },
+      providesTags: [tagTypes.department],
+    }),
+
+    addDepartment: build.mutation({
+      query: (data) => ({
+        url: DEPARTMENT_URL,
+        method: "POST",
+        data,
+      }),
+      invalidatesTags: [tagTypes.department],
+    }),
+  }),
+});
+
+export const { useDepartmentsQuery, useAddDepartmentMutation } = departmentApi;
